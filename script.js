8b785accffa9b5e7731665ebd46c09838a8baf6e
neharanistudio <nehach7867@gmail.com>
2026-07-16 11:57:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const sidebar = document.querySelector(".sidebar");
 const overlay = document.querySelector(".menu-overlay");
 
 
+// Mobile Menu
+
 if(menuIcon && sidebar && overlay){
 
     menuIcon.addEventListener("click", function(){
@@ -17,6 +19,67 @@ if(menuIcon && sidebar && overlay){
 
         sidebar.classList.remove("active");
         overlay.classList.remove("active");
+
+    });
+
+}
+
+
+
+
+
+// Close menu after clicking category
+
+const sidebarLinks = document.querySelectorAll(".sidebar a");
+
+
+sidebarLinks.forEach(link => {
+
+    link.addEventListener("click", function(){
+
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+
+    });
+
+});
+
+
+
+
+
+// Back To Top Button
+
+
+const topBtn = document.getElementById("topBtn");
+
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 400){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+
+
+if(topBtn){
+
+    topBtn.addEventListener("click", function(){
+
+        window.scrollTo({
+
+            top:0,
+            behavior:"smooth"
+
+        });
 
     });
 
